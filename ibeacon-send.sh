4c5706b25e61d902af5fca4d96a61b1cd33f28b9
wwwins <jacky.huang@isobar.com>
2016-01-15 11:39:43 +0800

    Added ibeacon sending tools.

---
+++ ibeacon-send.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# for csr 4.0 and AirLocate on iphone:
+# bluez-ibeacon 500 E2C56DB5DFFB48D2B060D0F5A71096E0 1 12 -45
+#
+# Hit ctrl-c to stop it or kill -INT pid
+#
+
+export UUID="E2C56DB5DFFB48D2B060D0F5A71096E0"
+export MAJOR="1"
+export MINOR="99"
+export POWER="-45"
+export ADTIME="500"
+
+echo "Launching virtual iBeacon..."
+/root/bin/bluez-ibeacon $ADTIME $UUID $MAJOR $MINOR $POWER &
+PID=$!
+sleep 1
+kill -INT $PID
+echo "Complete"
+
+
