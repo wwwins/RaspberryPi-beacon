cff671eb01e8a83ddd56989faf1d17c46f8cb0ce
wwwins <jacky.huang@isobar.com>
2016-02-23 17:47:37 +0800

    Added UUIDs.

--- ibeacon-sends.sh
+++ ibeacon-sends.sh
@@ -6,14 +6,17 @@
 # Hit ctrl-c to stop it or kill -INT pid
 #
 
-export UUID="E2C56DB5DFFB48D2B060D0F5A71096E0"
+#export UUID="E2C56DB5DFFB48D2B060D0F5A71096E0"
+# 0號機
+export UUID="0FDDEC8B6A17467E94E157EAC571D270"
+# 1號機
+export UUID="DEFE84E391AA464EA0B86B87736DBE31"
 export MAJOR="1"
-export MINOR="99"
+export MINOR="91"
 export POWER="-45"
 export ADTIME="500"
 
 echo "Launching virtual iBeacon..."
-/root/bin/bluez-ibeacon $ADTIME $UUID $MAJOR $MINOR $POWER 
+/usr/local/bin/hciconfig hci0 up
+/root/bin/bluez-ibeacon $ADTIME $UUID $MAJOR $MINOR $POWER
 echo "Complete"
-
-
